7821b44316fc86952310d2581a83fd41f3b113c9
Elon <13120612730@163.com>
2021-09-28 23:45:56 +0800

    Middle: 567.checkInclusion finished

--- checkInclusion/README.md
+++ checkInclusion/README.md
@@ -23,3 +23,52 @@
 ## 解答
 ### 方法一
 **解释**：
+可用滑动窗口法：  
+- 因为s1和s2中仅含小写字母，所以可以用长度为26的数组freq来代替map，
+- 先统计s1中每个字母出现的频率，记录在freq1中
+- 初始化freq2中每个元素的值为0
+- 增大窗口的右端high，当`freq2[s2[high] - 'a'] + 1 > freq1[s2[high] - 'a']`时，将low向右移动至`s[high]`首次出现的位置，并将沿途的字母（不包括`s[high]`）出现的频率清零
+- 当high - low == s1的长度时，返回true
+- 若high增长至s2右端时，若high - low == s1的长度，返回 true
+- 否则返回false
+**代码**：
+```go
+func checkInclusion(s1 string, s2 string) bool {
+	if s1 == "" || s2 == "nil" {
+		return false
+	}
+
+	freq1 := [26]int{0}
+	for _, c := range s1 {
+		freq1[byte(c)-'a']++
+	}
+	freq2 := [26]int{0}
+
+	low, high, l1, l2 := 0, 0, len(s1), len(s2)
+	for high < l2 {
+		if high-low == l1 {
+			return true
+		}
+		if t := freq2[s2[high]-'a'] + 1; t > freq1[s2[high]-'a'] {
+			i := low
+			for s2[i] != s2[high] {
+				freq2[s2[i]-'a']--
+				i++
+			}
+			i++
+			low = i
+
+		} else {
+			freq2[s2[high]-'a']++
+		}
+
+		high++
+	}
+
+	if high-low == l1 {
+		return true
+	}
+
+	return false
+}
+```
--- checkInclusion/checkInclusion.go
+++ checkInclusion/checkInclusion.go
@@ -1,8 +1,109 @@
 package main
 
+import "fmt"
+
+// 错误的，无法处理s1中字母重复出现的情况
+// func checkInclusion1(s1 string, s2 string) bool {
+// 	if s1 == "" || s2 == "nil" {
+// 		return false
+// 	}
+// 	exists := make(map[byte]int)
+
+// 	for _, c := range s1 {
+// 		exists[byte(c)] = -1
+// 	}
+// 	low, high, l1, l2 := 0, 0, len(s1), len(s2)
+// 	for high < l2 {
+// 		if high-low == l1 {
+// 			return true
+// 		}
+
+// 		if exists[s2[high]] != -1 {
+// 			t := exists[s2[high]]
+// 			if t != 0 {
+// 				i := low
+// 				for s2[i] != s2[high] {
+// 					exists[s2[i]] = -1
+// 					i++
+// 				}
+// 				low = i
+// 			} else {
+// 				low = high + 1
+// 				for _, c := range s1 {
+// 					exists[byte(c)] = -1
+// 				}
+// 			}
+// 		} else {
+// 			exists[s2[high]] = high + 1
+// 		}
+// 		high++
+// 	}
+
+// 	if high-low == l1 {
+// 		return true
+// 	}
+
+// 	return false
+// }
+
 func checkInclusion(s1 string, s2 string) bool {
 	if s1 == "" || s2 == "nil" {
 		return false
 	}
 
+	freq1 := [26]int{0}
+	for _, c := range s1 {
+		freq1[byte(c)-'a']++
+	}
+	freq2 := [26]int{0}
+
+	low, high, l1, l2 := 0, 0, len(s1), len(s2)
+	for high < l2 {
+		if high-low == l1 {
+			return true
+		}
+		if t := freq2[s2[high]-'a'] + 1; t > freq1[s2[high]-'a'] {
+			i := low
+			for s2[i] != s2[high] {
+				freq2[s2[i]-'a']--
+				i++
+			}
+			i++
+			low = i
+
+		} else {
+			freq2[s2[high]-'a']++
+		}
+
+		high++
+	}
+
+	if high-low == l1 {
+		return true
+	}
+
+	return false
+}
+
+func main() {
+	tests := []struct {
+		s1, s2 string
+		res    bool
+	}{
+		{"ab", "eidbaooo", true},
+		{"ab", "eidboaooo", false},
+		{"adc", "dcda", true},
+		{"ab", "ab", true},
+		{"hello", "ooolleoooleh", false},
+	}
+
+	for _, test := range tests {
+		res := checkInclusion(test.s1, test.s2)
+		fmt.Printf("checkInclusion(%q,%q) = %t: ", test.s1, test.s2, res)
+		if test.res != res {
+			fmt.Println("failed")
+		} else {
+			fmt.Println("passed")
+		}
+	}
 }
